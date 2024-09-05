b533d0e68b2e2fea99e8419c687067599c8ce253
gaoguangxiao <gaoguangxiao125@sina.com>
2024-09-05 18:08:39 +0800

    脚本导包和部署

--- REAMD.md
+++ REAMD.md
@@ -1,4 +1,13 @@
+1.配置本地nginx并支持ssl。
+2.ipa安装配置以及下载网页
+3.配置Jenkins
+- ExportXbuild：打包配置
+- service：ssl证书和服务器配置，将服务器配置替换，ssl文件夹复制到nginx目录下
+
 1.brew安装nigix
 2.在home目录git clone【必须】
-3.将服务器nginx.conf替换仓库
-4.ssl文件夹复制到nginx目录下
+3.`config.conf`文件用于生成自签名证书，生成目录位于user目录
+4.执行config.sh命令初始化配置
+
+4.配置Jenkins，可执行Jenkins.sh脚本配置
+5.Jenkins展示下载地址
--- config.sh
+++ config.sh
@@ -1,5 +1,30 @@
-# 创建文件夹
-mkdir usr/local/var/sjenkins
+# 创建文件夹，服务器路径写绝对，因此前缀不可包含用户名。路径要公用，nginx默认路径在www文件夹下
+# mkdir /usr/local/var/sjenkins
+# echo '创建文件夹'
 
-# 替换服务器目录
+# 配置服务器路径
+SOURCE_PATH=./service/sjenkins
 
+cp -a -f ${SOURCE_PATH} /usr/local/var/
+
+# 替换服务器配置
+# 源目录
+SOURCE_SSL=./service/ssl
+SOURCE_Conf=./service/nginx.conf
+#目的目录
+DESTINATION_SSL=/opt/homebrew/etc/nginx
+DESTINATION_Conf=/opt/homebrew/etc/nginx/nginx.conf
+
+# /usr/local/etc/nginx/nginx.conf
+# M1 系统路径
+# /opt/homebrew/etc/nginx/nginx.conf
+
+cp -a -f ${SOURCE_SSL} ${DESTINATION_SSL}
+
+# 替换conf文件
+cp -a -f ${SOURCE_Conf} ${DESTINATION_SSL}
+
+# 刷新nginx
+nginx -s reload
+
+brew services restart nginx
